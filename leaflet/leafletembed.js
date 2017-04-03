389cf3de4294c84c65eefb21c921e110a01357f3
Gorka Revilla <gorkarevilla@gmail.com>
2017-04-03 13:07:13 +0200

    use local jquery and add map

---
+++ leaflet/leafletembed.js
@@ -0,0 +1,19 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() {
+	// set up the map
+	map = new L.Map('map');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	// Eibar: 43.1848,-2.4790
+	map.setView(new L.LatLng(43.1848, -2.4790),13);
+	map.addLayer(osm);
+}
